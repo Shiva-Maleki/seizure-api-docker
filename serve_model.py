c984b6700e6b47c79d20f7dead6a4fc8dad292b1
Shiva Maleki Varnosfaderani <149103056+Shiva-Maleki@users.noreply.github.com>
2025-04-09 19:51:15 -0400

    Add files via upload

--- serve_model.py
+++ serve_model.py
@@ -24,6 +24,6 @@ def predict():
     return jsonify({"prediction": prediction})
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
    
 
